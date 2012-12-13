b99688380bd77ae1dd3dd2f049b1011f0531a573
David Duffett <duffett.david@gmail.com>
2012-12-13 10:09:03 +0000

    Simplify solution (odd reference issue when building via command line)

---
+++ Examples/WebFormsApplication/ApplicationAssemblyDetails.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using eSpares.Levity;
+
+namespace WebFormsApplication
+{
+    public class ApplicationAssemblyDetails
+    {
+        public static ApplicationAssemblyDetails Current
+        {
+            get
+            {
+                return new ApplicationAssemblyDetails
+                {
+                    ApplicationAssembly = ApplicationAssemblyUtility.ApplicationAssembly,
+                    VersionNumber = ApplicationAssemblyUtility.GetApplicationVersionNumber(),
+                    BinFolder = ApplicationAssemblyUtility.ApplicationBinFolder,
+                    BuildMode = ApplicationAssemblyUtility.ApplicationIsDebugBuild() ? "Debug" : "Release"
+                };
+            }
+        }
+
+        public Assembly ApplicationAssembly { get; set; }
+        public string VersionNumber { get; set; }
+        public string BinFolder { get; set; }
+        public string BuildMode { get; set; }
+    }
+}
