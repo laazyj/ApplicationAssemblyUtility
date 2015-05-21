d1eb7747d3709a3534e53ae92ab92abdafca7b74
Alex Cantù <acantu@geiweb.it>
2015-05-21 16:36:52 +0200

    updated examples

--- Examples/WebFormsApplication/ApplicationAssemblyDetails.cs
+++ Examples/WebFormsApplication/ApplicationAssemblyDetails.cs
@@ -19,6 +19,20 @@ namespace WebFormsApplication
             }
         }
 
+        public static ApplicationAssemblyDetails Current00
+        {
+            get
+            {
+                return new ApplicationAssemblyDetails
+                {
+                    ApplicationAssembly = ApplicationAssemblyUtility.ApplicationAssembly,
+                    VersionNumber = ApplicationAssemblyUtility.GetApplicationVersionNumber(true),
+                    BinFolder = ApplicationAssemblyUtility.ApplicationBinFolder,
+                    BuildMode = ApplicationAssemblyUtility.ApplicationIsDebugBuild() ? "Debug" : "Release"
+                };
+            }
+        }
+
         public Assembly ApplicationAssembly { get; set; }
         public string VersionNumber { get; set; }
         public string BinFolder { get; set; }
--- Examples/WebFormsApplication/Global.asax.cs
+++ Examples/WebFormsApplication/Global.asax.cs
@@ -5,10 +5,12 @@ namespace WebFormsApplication
     public class Global : System.Web.HttpApplication
     {
         public static ApplicationAssemblyDetails ApplicationAssemblyDetails { get; set; }
+        public static ApplicationAssemblyDetails ApplicationAssemblyDetails00 { get; set; }
 
         void Application_Start(object sender, EventArgs e)
         {
             ApplicationAssemblyDetails = ApplicationAssemblyDetails.Current;
+            ApplicationAssemblyDetails00 = ApplicationAssemblyDetails.Current00;
         }
     }
 }
